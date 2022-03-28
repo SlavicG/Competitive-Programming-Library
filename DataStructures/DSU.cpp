dade4c2e1eeaf86cc3384d14a82dc69c743044b3
SlavicG <68696994+SlavicG@users.noreply.github.com>
2022-03-28 16:58:31 +0300

    Update DSU.cpp

--- DataStructures/DSU.cpp
+++ DataStructures/DSU.cpp
@@ -1,21 +1,14 @@
-struct DSU{
+struct DSU {
     vector<int> p;
     DSU(int n) {p.assign(n, -1);}
-    int get(int a) {
-        return p[a] < 0 ? a : p[a] = get(p[a]);
-    }
+    int get(int a) {return p[a] < 0 ? a : p[a] = get(p[a]);}
     bool uni(int a, int b) {
-        a = get(a), b = get(b);
+        a = get(a), b = get(b); 
         if(a == b) return false;
-        if(p[a] > p[b]) swap(a, b);
-        p[a] += p[b]; 
-        p[b] = a;
+        if(p[a] > p[b]) swap(a, b); 
+        p[a] += p[b]; p[b] = a;
         return true;
     }
-    int getsize(int a) {
-        return -p[get(a)];
-    }
-    bool sameset(int a, int b) {
-        return (get(a) == get(b));
-    }
+    int getsize(int a) {return -p[get(a)];}
+    bool sameset(int a, int b) {return (get(a) == get(b));}
 };
