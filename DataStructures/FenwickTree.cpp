574c5e8bb26f88a5a7ac450569fc9111d11385c5
SlavicG <68696994+SlavicG@users.noreply.github.com>
2022-04-07 09:13:09 +0300

    Update FenwickTree.cpp

--- DataStructures/FenwickTree.cpp
+++ DataStructures/FenwickTree.cpp
@@ -8,28 +8,18 @@ struct fenwick_tree {
     fenwick_tree(vector<T> a) {
         N = a.size();
         fen.assign(N + 1, 0);
-        for(int i = 0; i < N; ++i) {
-            add(i, a[i]);
-        }
+        for(int i = 0; i < N; ++i) add(i, a[i]);
     }
     void add(int p, T val) {
-        for(int i = p + 1;i <= N;i += i & -i) {
-            fen[i] += val;
-        }
+        for(int i = p + 1;i <= N;i += i & -i) fen[i] += val;
     }
     void set(int p, T val) {
-        T set_val = val - query(p, p);
-        add(p, set_val);
+        T set_val = val - query(p, p); add(p, set_val);
     }
-
     T query(int l, int r) {
         T ret = 0;
-        for(int i = r + 1; i >= 1; i -= i & -i) {
-            ret += fen[i];
-        }   
-        for(int i = l; i >= 1; i -= i & -i) {
-            ret -= fen[i];
-        }   
+        for(int i = r + 1; i >= 1; i -= i & -i) ret += fen[i];
+        for(int i = l; i >= 1; i -= i & -i) ret -= fen[i];
         return ret;
     }
 };
